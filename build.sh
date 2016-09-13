f33d6395cfab80e2ea597fd0de1e7202bb568dd4
Alex A. Mihaylov <alex_a_mihaylov@yahoo.com>
2016-09-13 11:01:58 +0300

    Add ravion specific build script and defconfig

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+export CROSS_COMPILE=armv7a-neon-linux-gnueabi-
+export ARCH=arm
+export ROOT_FS_PATH=/cimc/root/armv7a-neon/exports
+export TFTP_FS_PATH=/cimc/exporttftp
+export DEF_TARGET="zImage modules vf500-colibri-eval-v3.dtb vf610-colibri-eval-v3.dtb"
+export DEF_ARGS="-j3 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} \
+INSTALL_MOD_PATH=${ROOT_FS_PATH}"
+export SUDO=sudo
+
+if [ -z "$*" ]; then
+    [ -f .config ] || make ${DEF_ARGS} colibri_vf_ravion_defconfig
+    make ${DEF_ARGS} ${DEF_TARGET}
+    echo -n Cleanup old modules...
+    ${SUDO} rm -rf ${ROOT_FS_PATH}/lib/modules/* && echo OK || echo FAIL
+    ${SUDO} make ${DEF_ARGS} modules_install
+    echo install kernel into rootfs and tftp
+    ${SUDO} cp -f arch/arm/boot/zImage ${ROOT_FS_PATH}/boot/zImage
+    ${SUDO} cp -f arch/arm/boot/dts/vf500-colibri-eval-v3.dtb ${ROOT_FS_PATH}/boot/vf500.dtb
+    ${SUDO} cp -f arch/arm/boot/dts/vf610-colibri-eval-v3.dtb ${ROOT_FS_PATH}/boot/vf610.dtb
+    ${SUDO} cp -f arch/arm/boot/zImage ${TFTP_FS_PATH}/boot/zImage
+    ${SUDO} cp -f arch/arm/boot/dts/vf500-colibri-eval-v3.dtb ${TFTP_FS_PATH}/vf500.dtb
+    ${SUDO} cp -f arch/arm/boot/dts/vf610-colibri-eval-v3.dtb ${TFTP_FS_PATH}/vf610.dtb
+else
+    make ${DEF_ARGS} $*
+fi
