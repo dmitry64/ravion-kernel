53103e96eb623f48d708fcd094b58a143d19a82c
Alex A. Mihaylov <alex_a_mihaylov@yahoo.com>
2016-10-31 11:58:06 +0300

    Apply ravion MCP patches

--- build.sh
+++ build.sh
@@ -5,33 +5,29 @@ export ARCH=arm
 export ROOT_FS_PATH=/cimc/root/armv7a-neon/exports
 export TFTP_FS_PATH=/cimc/exporttftp
 export BR_OVERLAY_PATH=/cimc/build/__git__/buildroot/output/overlay
-export DEF_TARGET="zImage modules imx6dl-colibri-cam-eval-v3.dtb imx6dl-colibri-eval-v3.dtb"
+export DEF_TARGET="zImage modules imx6dl-colibri-mcp.dtb"
 export DEF_ARGS="-j3 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} \
 INSTALL_MOD_PATH=${ROOT_FS_PATH}"
 export SUDO=sudo
 
 if [ -z "$*" ]; then
-    [ -f .config ] || make ${DEF_ARGS} colibri_imx6_defconfig
+    [ -f .config ] || make ${DEF_ARGS} ravion_imx6_defconfig
     make ${DEF_ARGS} ${DEF_TARGET}
     echo -n Cleanup old modules...
     ${SUDO} rm -rf ${ROOT_FS_PATH}/lib/modules/* && echo OK || echo FAIL
     ${SUDO} make ${DEF_ARGS} modules_install
     echo install kernel into rootfs and tftp
     ${SUDO} cp -f arch/arm/boot/zImage ${ROOT_FS_PATH}/boot/zImage
-    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-cam-eval-v3.dtb ${ROOT_FS_PATH}/boot/mx6-cam.dtb
-    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-eval-v3.dtb ${ROOT_FS_PATH}/boot/mx6.dtb
+    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-mcp.dtb ${ROOT_FS_PATH}/boot/mx6.dtb
     ${SUDO} cp -f arch/arm/boot/zImage ${TFTP_FS_PATH}/boot/zImage
-    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-cam-eval-v3.dtb ${TFTP_FS_PATH}/boot/mx6-cam.dtb
-    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-eval-v3.dtb ${TFTP_FS_PATH}/boot/mx6.dtb
+    ${SUDO} cp -f arch/arm/boot/dts/imx6dl-colibri-mcp.dtb ${TFTP_FS_PATH}/boot/mx6.dtb
     if [ -d ${BR_OVERLAY_PATH} ]; then
 	rm -rf \
 	${BR_OVERLAY_PATH}/lib/modules/* \
 	${BR_OVERLAY_PATH}/boot/zImage  \
-	${BR_OVERLAY_PATH}/boot/mx6-cam.dtb \
 	${BR_OVERLAY_PATH}/boot/mx6.dtb
 	cp -rf ${ROOT_FS_PATH}/lib/modules/*	${BR_OVERLAY_PATH}/lib/modules/
 	cp ${ROOT_FS_PATH}/boot/zImage		${BR_OVERLAY_PATH}/boot/
-	cp ${ROOT_FS_PATH}/boot/mx6-cam.dtb	${BR_OVERLAY_PATH}/boot/
 	cp ${ROOT_FS_PATH}/boot/mx6.dtb 	${BR_OVERLAY_PATH}/boot/
     fi
 else
